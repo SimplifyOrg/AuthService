5551c4b3cb136954b04ac9f2ef88a48f693f59aa
abhisheken_f83306518a9b <abhishek.en@gmail.com>
2024-09-11 09:59:55 +0530

    Permissions enhancements

--- src/main/java/com/example/userservice/repository/PermissionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.userservice.repository;
-
-import com.example.userservice.models.Permission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PermissionRepository extends JpaRepository<Permission, Long > {
-}
